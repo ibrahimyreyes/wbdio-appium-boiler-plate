0f17bde745cb57aba6b904c8c0d73aee22c70bbf
ibrahimyreyes <ivrahim.reyes@gmail.com>
2026-01-01 22:29:46 +0800

    build: update WebdriverIO configuration for GUI and headless modes

--- wdio.local.conf.ts
+++ wdio.local.conf.ts
@@ -1,12 +1,17 @@
 import { config as sharedConfig } from './wdio.shared.conf.js'
 
+const isGui = process.env.GUI === 'true'
+const chromeArgs = isGui
+    ? ['--window-size=1920,1080']
+    : ['--headless=new', '--disable-gpu', '--window-size=1920,1080']
+
 export const config: WebdriverIO.Config = {
     ...sharedConfig,
     ...{
         capabilities: [{
             browserName: 'chrome',
             'goog:chromeOptions': {
-                args: ['headless', 'disable-gpu']
+                args: chromeArgs
             }
         }]
     }
